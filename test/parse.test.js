3f9295c87ab81542b91f02276e1be28a8e941edc
Garmash Nikolay <garmash.nikolay@gmail.com>
2016-11-16 09:23:16 +0100

    - add "setup" parser which splits setup lexemes into environments list - add test for "setup" parser

--- test/parse.test.js
+++ test/parse.test.js
@@ -2,12 +2,16 @@ let assert = require('assert');
 let parse = require('../lib/parse');
 let globalSplitter = require('../lib/parsers/global-splitter');
 let bio = require('../lib/parsers/bio');
+let setup = require('../lib/parsers/setup');
 
 let globalSplitterInput = require('./stubs/parse/global-splitter--input');
 let globalSplitterOutput = require('./stubs/parse/global-splitter--output');
 
 let bioCleanInput = require('./stubs/parse/bio-clean--input');
 let bioCleanOutput = require('./stubs/parse/bio-clean--output');
+
+let setupInput = require('./stubs/parse/setup--input');
+let setupOutput = require('./stubs/parse/setup--output');
 
 const {
   GLOBAL_PARSER_KEY,
@@ -34,6 +38,17 @@ describe('parse', () => {
       let tree = parse(bioCleanInput, parsersMap);
 
       assert.deepEqual(tree, bioCleanOutput);
+    });
+  });
+
+  describe('setup', () => {
+    it('splits lexemes list to "environment" contexts', () => {
+      const parsersMap = {
+        [GLOBAL_PARSER_KEY]: setup
+      };
+      let tree = parse(setupInput, parsersMap);
+
+      assert.deepEqual(tree, setupOutput);
     });
   });
 });
---
+++ test/stubs/parse/setup--output.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    type: 'environment',
+    content: [
+      { type: 'env-bullet', content: '--' },
+      { type: 'word', content: 'desktop' },
+      { type: 'newline', content: '\n' },
+      { type: 'word', content: 'desktop' },
+      { type: 'word', content: 'env' },
+      { type: 'word', content: 'description' },
+      { type: 'newline', content: '\n' },
+      { type: 'comment-bullet', content: '//' },
+      { type: 'word', content: 'comment' },
+      { type: 'word', content: 'one' },
+      { type: 'newline', content: '\n' },
+      { type: 'tool-bullet', content: '>' },
+      { type: 'word', content: 'editor:' },
+      { type: 'word', content: 'Sublime' },
+      { type: 'word', content: 'Text' },
+      { type: 'word', content: '3' },
+      { type: 'newline', content: '\n' },
+      { type: 'word', content: 'tool' },
+      { type: 'word', content: 'description' },
+      { type: 'newline', content: '\n' }
+    ]
+  },
+  {
+    type: 'environment',
+    content: [
+      { type: 'env-bullet', content: '--' },
+      { type: 'word', content: 'mobile' },
+      { type: 'newline', content: '\n' },
+      { type: 'tool-bullet', content: '>' },
+      { type: 'word', content: 'browser:' },
+      { type: 'word', content: 'Safari' }
+    ]
+  }
+];
