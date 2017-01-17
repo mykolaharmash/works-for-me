cbd496ed7bdd90ff0c9bcf78e66f52c58cf19494
Garmash Nikolay <garmash.nikolay@gmail.com>
2017-01-17 09:54:32 +0100

    Environment renderer
    adds environment renderer

---
+++ test/render-environment.test.js
@@ -0,0 +1,23 @@
+let fs = require('fs');
+let assert = require('assert');
+let render = require('../lib/render');
+let environment = require('../lib/renderers/environment');
+
+const input = require('./stubs/render/environment--input');
+const output = fs
+  .readFileSync(require.resolve('./stubs/render/environment--output.html'))
+  .toString();
+
+const { ENVIRONMENT_RENDERER_KEY } = require('../lib/constants');
+
+describe('bio renderer', () => {
+  const renderersMap = {
+    [ENVIRONMENT_RENDERER_KEY]: environment
+  };
+
+  it('renders empty environment context', () => {
+    let html = render(input, renderersMap);
+
+    assert.equal(html, output);
+  });
+});
---
+++ test/stubs/render/environment--input.js
@@ -0,0 +1,6 @@
+const { ENVIRONMENT_CONTEXT } = require('../../../lib/constants');
+
+module.exports = {
+  type: ENVIRONMENT_CONTEXT,
+  content: []
+};
