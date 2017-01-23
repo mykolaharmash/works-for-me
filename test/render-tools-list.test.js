c5186532238a154f620fe22bfd3fe405b30a7229
Garmash Nikolay <garmash.nikolay@gmail.com>
2017-01-23 10:56:23 +0100

    Tools list renderer
    adds tools list renderer

---
+++ test/render-tools-list.test.js
@@ -0,0 +1,23 @@
+let fs = require('fs');
+let assert = require('assert');
+let render = require('../lib/render');
+let toolsList = require('../lib/renderers/tools-list');
+
+const input = require('./stubs/render/tools-list--input');
+const output = fs
+  .readFileSync(require.resolve('./stubs/render/tools-list--output.html'))
+  .toString();
+
+const { TOOLS_LIST_RENDERER_KEY } = require('../lib/constants');
+
+describe('tools list renderer', () => {
+  const renderersMap = {
+    [TOOLS_LIST_RENDERER_KEY]: toolsList
+  };
+
+  it('renders empty tools list context', () => {
+    let html = render(input, renderersMap);
+
+    assert.equal(html, output);
+  });
+});
---
+++ test/stubs/render/tools-list--input.js
@@ -0,0 +1,6 @@
+const { TOOLS_LIST_CONTEXT } = require('../../../lib/constants');
+
+module.exports = {
+  type: TOOLS_LIST_CONTEXT,
+  content: []
+};
