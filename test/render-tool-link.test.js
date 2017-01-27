934431a468ed6fc7284ed446605db4b43d0e60f2
Garmash Nikolay <garmash.nikolay@gmail.com>
2017-01-27 16:18:36 +0100

    Tool link renderer
    adds tool link renderer to render url as a plain text
    adds tests for tool link renderer

---
+++ test/render-tool-link.test.js
@@ -0,0 +1,21 @@
+let fs = require('fs');
+let assert = require('assert');
+let render = require('../lib/render');
+let toolLink = require('../lib/renderers/tool-link');
+
+const input = require('./stubs/render/tool-link--input');
+const output = fs.readFileSync(require.resolve('./stubs/render/tool-link--output.html'), 'utf8');
+
+const { TOOL_LINK_RENDERER_KEY } = require('../lib/constants');
+
+describe('tool link renderer', () => {
+  const renderersMap = {
+    [TOOL_LINK_RENDERER_KEY]: toolLink
+  };
+
+  it('renders tool\'s url as plain text', () => {
+    let html = render(input, renderersMap);
+
+    assert.equal(html, output);
+  });
+});
---
+++ test/stubs/render/tool-link--input.js
@@ -0,0 +1,14 @@
+const {
+  TOOL_LINK_CONTEXT,
+  WORD_LEXEME
+} = require('../../../lib/constants');
+
+module.exports = {
+  type: TOOL_LINK_CONTEXT,
+  content: [
+    {
+      type: WORD_LEXEME,
+      content: 'http://some.url/test'
+    }
+  ]
+};
