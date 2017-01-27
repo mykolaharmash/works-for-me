b7d37b1fc4550c71fa2f940c15edcd27a9e27373
Garmash Nikolay <garmash.nikolay@gmail.com>
2017-01-27 14:48:03 +0100

    Tool name renderer
    modifies tool name renderer to use raw content of title
    and link contexts
    
    adds test for rendering name with title only

--- test/render-tool-name.test.js
+++ test/render-tool-name.test.js
@@ -4,9 +4,10 @@ let render = require('../lib/render');
 let toolName = require('../lib/renderers/tool-name');
 
 const inputEmpty = require('./stubs/render/tool-name-empty--input');
-const outputEmpty = fs
-  .readFileSync(require.resolve('./stubs/render/tool-name-empty--output.html'))
-  .toString();
+const outputEmpty = fs.readFileSync(require.resolve('./stubs/render/tool-name-empty--output.html'), 'utf8');
+
+const inputOnlyTitle = require('./stubs/render/tool-name-only-title--input');
+const outputOnlyTitle = fs.readFileSync(require.resolve('./stubs/render/tool-name-only-title--output.html'), 'utf8');
 
 const { TOOL_NAME_RENDERER_KEY } = require('../lib/constants');
 
@@ -15,9 +16,15 @@ describe('tool name renderer', () => {
     [TOOL_NAME_RENDERER_KEY]: toolName
   };
 
-  it('renders empty tool name', () => {
+  it('renders empty tool name as block', () => {
     let html = render(inputEmpty, renderersMap);
 
     assert.equal(html, outputEmpty);
+  });
+
+  it('renders name with title only as block', () => {
+    let html = render(inputOnlyTitle, renderersMap);
+
+    assert.equal(html, outputOnlyTitle);
   });
 });
---
+++ test/stubs/render/tool-name-only-title--input.js
@@ -0,0 +1,14 @@
+const {
+  TOOL_NAME_CONTEXT,
+  TOOL_TITLE_CONTEXT
+} = require('../../../lib/constants');
+
+module.exports = {
+  type: TOOL_NAME_CONTEXT,
+  content: [
+    {
+      type: TOOL_TITLE_CONTEXT,
+      content: 'tool title'
+    }
+  ]
+};
