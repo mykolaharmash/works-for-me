13671750f3722cc4a14d2e92e81bc0b90ed0cc35
Garmash Nikolay <garmash.nikolay@gmail.com>
2017-05-02 11:20:24 +0200

    RSS item renderer
    — creates renderer

---
+++ test/render-rss-item.test.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const fs = require('fs');
+const { assert } = require('chai');
+
+const render = require('../lib/render');
+const rssItemRenderer = require('../lib/renderers/rss-item');
+const {
+  RSS_ITEM_RENDERER_KEY
+} = require('../lib/constants');
+
+const input = require('./stubs/render/rss-item--input');
+const output = fs.readFileSync(path.resolve(__dirname, './stubs/render/rss-item--output.xml'), 'utf8');
+
+const renderersMap = {
+  [RSS_ITEM_RENDERER_KEY]: rssItemRenderer
+};
+
+describe('rss item renderer', () => {
+  it('renders wrapper for rss item', () => {
+    let xml = render(input, renderersMap);
+
+    assert.deepEqual(xml, output);
+  });
+});
---
+++ test/stubs/render/rss-item--input.js
@@ -0,0 +1,14 @@
+const {
+  RSS_ITEM_CONTEXT,
+  RSS_ITEM_TITLE_CONTEXT
+} = require('../../../lib/constants');
+
+module.exports = {
+  type: RSS_ITEM_CONTEXT,
+  content: [
+    {
+      type: RSS_ITEM_TITLE_CONTEXT,
+      content: '<title>item title</title>'
+    }
+  ]
+};
