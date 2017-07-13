b06e5fdabd8f3e33a26b3518e3648c197c7279a4
Garmash Nikolay <garmash.nikolay@gmail.com>
2017-07-13 09:35:47 +0200

    Sort setups list
    — adds sorting "latest first" for setups list
      on the main page

--- generate/setups-list-ast.js
+++ generate/setups-list-ast.js
@@ -1,4 +1,4 @@
-const Immutable = require('immutable');
+const Immutable = require('immutable')
 const {
   BIO_CONTEXT,
   BODY_CONTEXT,
@@ -8,76 +8,91 @@ const {
   SETUPS_LIST_CONTEXT,
   SETUP_UPDATE_DATE_CONTEXT,
   SETUP_CREATE_DATE_CONTEXT
-} = require('../lib/constants');
+} = require('../lib/constants')
 
 function findBioContext (setupAst) {
   let bodyContext = setupAst
     .get('content') // AST content
     .get('content') // root-context content
-    .find(context => context.get('type') === BODY_CONTEXT);
+    .find(context => context.get('type') === BODY_CONTEXT)
 
   if (!bodyContext) {
-    return;
+    return
   }
 
   return bodyContext
     .get('content')
-    .find(context => context.get('type') === BIO_CONTEXT);
+    .find(context => context.get('type') === BIO_CONTEXT)
 }
 
 function generateMetadataContexts (key, metadata) {
-  let contextsList = [];
+  let contextsList = []
 
   if (key) {
     contextsList.push({
       type: SETUPS_LIST_ITEM_KEY_CONTEXT,
       content: key
-    });
+    })
   }
 
   if (!metadata) {
-    return contextsList;
+    return contextsList
   }
 
   if (metadata.updates.length) {
     contextsList.push({
       type: SETUP_UPDATE_DATE_CONTEXT,
       content: metadata.updates[0].date
-    });
+    })
   } else {
     contextsList.push({
       type: SETUP_CREATE_DATE_CONTEXT,
       content: metadata.initial.date
-    });
+    })
   }
 
-  return contextsList;
+  return contextsList
 }
 
 function generateSetupListItem (setupAst, setupMetadata) {
-  let bio = findBioContext(setupAst);
-  let key = setupAst.get('name');
-  let content = [];
+  let bio = findBioContext(setupAst)
+  let key = setupAst.get('name')
+  let content = []
 
-  let metadataContexts = generateMetadataContexts(key, setupMetadata);
+  let metadataContexts = generateMetadataContexts(key, setupMetadata)
 
-  content = content.concat(metadataContexts);
-  content.push(bio);
+  content = content.concat(metadataContexts)
+  content.push(bio)
 
   return Immutable.fromJS({
     type: SETUPS_LIST_ITEM_CONTEXT,
     content
-  });
+  })
+}
+
+function findDateContext (tree) {
+  return tree.get('content').find((context) => {
+    return context.get('type') === SETUP_CREATE_DATE_CONTEXT
+      || context.get('type') === SETUP_UPDATE_DATE_CONTEXT
+  })
+}
+
+function sortByDate (a, b) {
+  let aDateContext = findDateContext(a)
+  let bDateContext = findDateContext(b)
+
+  return Date.parse(aDateContext.get('content')) < Date.parse(bDateContext.get('content'))
 }
 
 function generate (setupsAstList = [], setupsMetadataList = []) {
   let setupsListItems = Immutable.fromJS(setupsAstList)
     .filter(setupAst => {
-      return findBioContext(setupAst) !== undefined;
+      return findBioContext(setupAst) !== undefined
     })
     .map((setupAst, index) => {
-      return generateSetupListItem(setupAst, setupsMetadataList[index]);
-    });
+      return generateSetupListItem(setupAst, setupsMetadataList[index])
+    })
+    .sort(sortByDate)
 
   return {
     type: SETUPS_LIST_ROOT_CONTEXT,
@@ -87,7 +102,7 @@ function generate (setupsAstList = [], setupsMetadataList = []) {
         content: setupsListItems.toJS()
       }
     ]
-  };
+  }
 }
 
-module.exports = generate;
+module.exports = generate
